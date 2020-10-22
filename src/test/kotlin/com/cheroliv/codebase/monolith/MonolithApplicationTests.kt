4b856071c2bf4728d4daf0ee59dd1d446de6c0cf
cheroliv <cheroliv@laposte.net>
2020-10-22 23:01:16 +0200

    first commit

---
+++ src/test/kotlin/com/cheroliv/codebase/monolith/MonolithApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cheroliv.codebase.monolith
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonolithApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
